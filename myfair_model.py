086a8af64ec021b88c1d85e58d92a4bf7800a9e7
hugsclane <mail.hugocl@gmail.com>
2024-12-18 17:18:14 +1100

    roadblock with linear dist, off to make the config properly

--- myfair_model.py
+++ myfair_model.py
@@ -14,11 +14,12 @@ def risk_calculator(**kwargs):
     # first we check which kwargs are provided and then we generate the appropriate distribution
     if kwargs.get("lef_decomposed") == True:
         #NOTE: here that if lef_decomposed is not in kwargs, this will assert false.
-        lef_dist = lef_from_tef_vuln()
-        ## we will
+        lef_dist,gen_mag_dist = lef_from_tef_vuln(),gen_mag_dist(kwargs.get("mag_dist"))
+
     else:
         lef_dist,mag_dist = gen_lef_dist(kwargs.get("lef_dist")),gen_mag_dist(kwargs.get("mag_dist"))
-        risk_dist = monte_carlo_sim(lef_dist,mag_dist,kwargs.get("n_simulations"))
+    risk_dist = monte_carlo_sim(lef_dist,mag_dist,kwargs.get("n_simulations"))
+
     sns.set(style="whitegrid")
     risk_sorted = sorted(risk_dist, reverse=True)
     exceedance_prob = [(i + 1) / len(risk_sorted) for i in range(len(risk_sorted))]
@@ -35,6 +36,29 @@ def monte_carlo_sim(lef_dist,mag_dist,n_simulations):
         risk_instance = random_event*random_mag
         risk.append(risk_instance)
     return risk
+
+def gen_tef_dist(dist_dict):
+    ## -----------------------
+    # lef_from_tef_vuln has the following shape
+    # for TEF happens N times,
+    # {dist : linear , N: int}
+    # for a specific number of n opputunities of TE to occur with p of actually occuring
+    # {dist : binomial, ptef: float < 1, n: int > 0}
+    # event threat over average unit t time
+    # {dist : poisson, lmbda: float > 0, size: int > 0}
+    ## -----------------------
+    if dist_dict == None:
+        raise ValueError("No distribution provided, Check your input or kwargs")
+    dist = dist_dict.get("dist")
+    if dist == "linear":
+        ## bugger, the __size__ needs to be globally accesible here. Time to make the classes.
+        return [1]*dist_dict.get("N")
+    elif dist == "binomial":
+        return binomial_dist(dist_dict.get("ptef"),dist_dict.get("n"))
+    elif dist == "poisson":
+        return poisson_dist(dist_dict.get("lmbda"),dist_dict.get("size"))
+    else:
+        raise ValueError("Invalid distribution type")
 
 def gen_mag_dist(dist_dict):
     ## -----------------------
